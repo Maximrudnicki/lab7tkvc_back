078c62841d11d3155804d0d11920ca44b844678c
Maxim Rudnitskiy <rydnizkui@gmail.com>
2023-06-12 16:39:23 +0300

    lab 7 tkvc cors origin added for frontend on render.com 5.0

--- lab3_tkvc/settings.py
+++ lab3_tkvc/settings.py
@@ -47,16 +47,18 @@ INSTALLED_APPS = [
     'order',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://lab7-tkvc-front.onrender.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
-    "http://localhost:8082",
-    "http://127.0.0.1:8082",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://lab7-tkvc-front.onrender.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8080",
+#     "http://localhost:8081",
+#     "http://127.0.0.1:8081",
+#     "http://localhost:8082",
+#     "http://127.0.0.1:8082",
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
